5329cb8273669364a9a9dc7ce65b8bca0ad46489
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-02 12:39:49 +0530

    refactor: change project scope

--- angular-owl-carousel-example/e2e/src/app.e2e-spec.ts
+++ angular-owl-carousel-example/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angularfiebase-authentication app is running!');
+    expect(page.getTitleText()).toEqual('angular-owl-carousel-example app is running!');
   });
 
   afterEach(async () => {
--- angular-owl-carousel-example/karma.conf.js
+++ angular-owl-carousel-example/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angularfiebase-authentication'),
+      dir: require('path').join(__dirname, './coverage/angular-owl-carousel-example'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
